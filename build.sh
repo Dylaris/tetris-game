23edf8117d5664bc5f44417c0d76a32fa71067b9
Dylaris <3445818679@qq.com>
2025-08-30 17:37:59 +0800

    change file postfix

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -xe
 g++ -Wall -Wextra -Wno-missing-field-initializers \
     -ggdb                                         \
     -I./raylib-5.5_linux_amd64/include            \
-    -o main main.cc                               \
+    -o main main.cpp                              \
     -L./raylib-5.5_linux_amd64/lib -lraylib       \
     -Wl,-rpath=./raylib-5.5_linux_amd64/lib
